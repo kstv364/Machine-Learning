cb60fa04272475bc0cdf142900d839699f5dc110
Kaustav Chanda <43077742+kstv364@users.noreply.github.com>
2019-03-19 22:12:26 +0530

    Finalised

--- ann/MLP.py
+++ ann/MLP.py
@@ -27,7 +27,7 @@ def dphi(x):
     return phi(x)*(1-phi(x))
 
 df = pd.read_csv('data.csv')
-print(df)
+#print(df)
 
 np.random.seed(42)
 df = df.reindex(np.random.permutation(df.index))
@@ -47,8 +47,11 @@ u1 = [0]*(h+1)
 v2 = [0]*(C+1)
 u2 = [0]*(C+1)
 
-W1 = [[ random.random()*2 - 1 for _ in range(d+1)] for _ in range(h+1)]
-W2 = [[ random.random()*2 - 1 for _ in range(h+1)] for _ in range(C+1)]
+W1 = [[ random.random() for _ in range(d+1)] for _ in range(h+1)]
+W2 = [[ random.random() for _ in range(h+1)] for _ in range(C+1)]
+
+print(W1)
+print(W2)
 
 bias = 1
 
@@ -70,16 +73,19 @@ eta = 0.01
 
 f = open('Results.txt','w')
 
-def fit(x,y,epochs=100): #function to train Neural Net
+def fit(x,y,eta,x_test,y_test,epochs=100): #function to train Neural Net
     c = 0
     N = len(x)
     prevE = 0
     E = 0
+    m = 0
     while(epochs>0):
+
+        #eta = eta * (1/(1 + 0.00001*c)) #decrease learning rate
         
         c+=1
         print("Epochs : " ,c)
-        f.write("Epochs : {}\t".format(c))
+        f.write("Epoch : \t{}".format(c))
         prevE = E
         E = 0
         for n in range(1,N+1):
@@ -92,8 +98,7 @@ def fit(x,y,epochs=100): #function to train Neural Net
             e = 0
             for k in range(1,C+1):
                 e += (y[n-1][k-1] - v2[k])**2
-            if(e<0.01):
-                break
+            
             e/=2
 
             
@@ -111,10 +116,27 @@ def fit(x,y,epochs=100): #function to train Neural Net
                     W1[j][i] = W1[j][i] + eta*sum([(y[n-1][k-1] - v1[k])*dphi(u2[k])*W2[k][j]*dphi(u1[j])*x[n-1][i-1] for k in range(1,C+1)])
                 W1[j][0] = W1[j][0] + eta*sum([(y[n-1][k-1] - v1[k])*dphi(u2[k])*W2[k][j]*dphi(u1[j])*bias for k in range(1,C+1)])
         #epochs-=1
-        print("Error : {}\n".format(E))
+        print("Fold ({}) : Error : {}\n".format(fold,E))
         f.write("Error : {}\n".format(E))
-        if(abs(E-prevE)<0.000001):
+
+        #breaking conditions
+        acc = predict(x_test,y_test)
+
+
+        if(acc==1.0 or(acc>0.9 and c>2000) or c>3000):
             break
+
+
+        if(E>prevE):
+            m+=1
+
+        if(m>100): #tolerance value
+
+            print("Stuck in local minima")
+            f.write("Stuck in local minima\n")
+            break
+
+        
         
 
 def predict(x,y):
@@ -132,24 +154,35 @@ def predict(x,y):
         for k in range(1,C+1):
             u22[k] = sum([v11[j]*W2[k][j] for j in range(1,h+1)]) + bias*W2[k][0]
             v22[k] = phi(u22[k])
-        # e = 0
-        # for k in range(1,C+1):
-        #     e += (y[n-1][k-1] - v22[k])**2
-        # e/=2
-        #print(np.argmax(y[n-1])+1,':',np.argmax(v22))
-        e = (np.argmax(y[n-1])+1 == np.argmax(v22)).astype(int)
+        
+
+        e = ((np.argmax(y[n-1])) == np.argmax(v22[1:])).astype(int)
         E += e
     E/=N
+
+    return E
+
+
+
+from sklearn.model_selection import KFold
+
+kf = KFold(5)
+fold = 0
+for train, test in kf.split(x):
+    fold+=1
+    print("Fold #{}\t:".format(fold))
+    f.write("Fold #{}\t:\n\n".format(fold))
+        
+    x_train = x[train]
+    y_train = y[train]
+    x_test = x[test]
+    y_test = y[test]
+
+    fit(x_train,y_train,eta,x_test,y_test)
+    E = predict(x_test,y_test)
+
     print("Accuracy : {}".format(E))
-    f.write("Accuracy : {}".format(E))
-
-from sklearn.model_selection import train_test_split
-x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.25)
-
-print(x_train)
-print(y_train)
-
-fit(x_train,y_train)
-predict(x_test,y_test)
-
+    f.write("\nWeights\t:\n")
+    f.write("{}\n{}\n\n".format(W1,W2))
+    f.write("Accuracy : {}\n".format(E))
 f.close()
